14e2e35ce42cbc9cbe148e33944fac1dddc6ad50
CyberSkam <saikrishna001207@gmail.com>
2020-04-02 23:11:25 +0530

    Basic node stuff

---
+++ Calculator-WebDev/calculator.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var result = n1 * n2;
+    res.send("The result of the calculation is: " + result);
+});
+
+app.get("/bmiCalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res) {
+    var h = parseFloat(req.body.height);
+    var w = parseFloat(req.body.weight);
+    var bmi = w / (h * h);
+    res.send("Your BMI is : " + bmi);
+})
+
+app.listen(3000, function() {
+    console.log("Server has started on port 3000");
+});
